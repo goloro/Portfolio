b9879025bc23c7c31b6a1ff174e8279f5e02cc17
goloro <goloro02@gmail.com>
2021-10-30 22:54:46 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// LENGUAJES PROJECT 1
+const js = document.getElementById("JS1")
+const css = document.getElementById("CSS1")
+const html = document.getElementById("HTML1")
+// BUTTON JS
+function ONhoverLenguajesJS() {
+    js.innerHTML = "JavaScript"
+}
+function OFFhoverLenguajesJS() {
+    js.innerHTML = ""
+}
+// BUTTON CSS
+function ONhoverLenguajesCSS() {
+    css.innerHTML = "CSS"
+}
+function OFFhoverLenguajesCSS() {
+    css.innerHTML = ""
+}
+// BUTTON HTML
+function ONhoverLenguajesHTML() {
+    html.innerHTML = "HTML"
+}
+function OFFhoverLenguajesHTML() {
+    html.innerHTML = ""
+}
