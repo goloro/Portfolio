33ba4c2a209e4cba2d0fa68e6a919c4ee0f08d6b
goloro <goloro02@gmail.com>
2022-01-14 22:53:56 +0100

    first commit

--- script.js
+++ script.js
@@ -11,24 +11,36 @@ function iniciarMapa() {
     })
 }
 
-document.getElementById("BarraMenuRes").addEventListener("click", function() {
-    document.getElementById("subMenuRes").classList.toggle("showMenuRes")
-    // document.querySelector(".subBody").classList.toggle("filtro")
-    // let menu = document.getElementById("subMenuRes")
-    // if ( menu.style.display != "none") {
-    //     menu.style.display = "flex"
-    // } else {
-    //     menu.style.display = "none"
-    // }
+// Función menu responsive
+const menuRes = document.getElementById("barraMenuRes")
+menuRes.addEventListener("click", function() {  
+    let subMenu = document.getElementById("subMenuRes")
+    let display = subMenu.style.display
+    if (display == "flex") {
+        MenuBlack(subMenu)
+    }
+    else {
+        MenuWhite(subMenu)
+    }
 })
 
-document.querySelector(".itemRes").addEventListener("click", function() {
-    // document.querySelector(".subBody").classList.toggle("filtro")
-    document.getElementById("subMenuRes").classList.toggle("showMenuRes")
-    // document.getElementById("subMenuRes").style.display = "none"
+document.getElementById("subMenuRes").addEventListener("click", e => {
+    alert(e.target.tagName)
+    if (e.target.tagName == "P")
+        MenuBlack(document.getElementById("subMenuRes"))
 })
 
-setTimeout(function() {
-    document.getElementById("inicio").style.display = "none"
-    document.getElementById("subBody").style.display = "flex"
-}, 2000)
+function MenuBlack(subMenu) {
+    subMenu.style.display = "none"
+    document.querySelector("nav").style.backgroundColor = "#1C1B24"
+    document.getElementById("logoNavImg").src = "Images/logo_glr_blue.png"
+    document.getElementById("nombreNav").style.color = "white"
+    menuRes.src = "Images/menu.svg"
+}
+function MenuWhite(subMenu) {
+    subMenu.style.display = "flex"
+    document.querySelector("nav").style.backgroundColor = "white"
+    document.getElementById("logoNavImg").src = "Images/logo_glr_dark.png"
+    document.getElementById("nombreNav").style.color = "#1C1B24"
+    menuRes.src = "Images/menu_dark.svg" 
+}
